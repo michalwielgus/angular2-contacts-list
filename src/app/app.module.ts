b5b68c088e385fde71c8484387a0db53bf0be9c3
Michał Wielgus <wielgus.michaal@gmail.com>
2016-11-26 18:58:05 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { ContactItemComponent } from './contact-item/contact-item.component';
+import { GravatarComponent } from './gravatar/gravatar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsListComponent,
+    ContactItemComponent,
+    GravatarComponent
   ],
   imports: [
     BrowserModule,
